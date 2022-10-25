5f3e979f500e155d3f4d743c11e34745133e220e
Bram Reuling <bram.reuling@outlook.com>
2022-10-25 09:47:46 +0200

    Added base project

---
+++ ValidationProject/Source/ValidationProject/ValidationProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ValidationProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALIDATIONPROJECT_API AValidationProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
